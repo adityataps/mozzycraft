6a6184cb7a98634f044f7305214542fcee80d018
Aditya Tapshalkar <39311849+adityataps@users.noreply.github.com>
2024-05-22 19:40:01 -0700

    init

---
+++ versions.tf
@@ -0,0 +1,16 @@
+terraform {
+  required_version = ">= 1.8.4"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 5.0"
+    }
+  }
+  cloud {
+    organization = "adi-sandbox"
+    workspaces {
+      name = "mozzycreek"
+    }
+  }
+}
