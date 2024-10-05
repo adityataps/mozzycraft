bb5a03d0e3e70f70e379c368525061f71ae3b25e
Aditya Tapshalkar <aditya.taps@gmail.com>
2024-10-05 19:19:27 -0400

    hard reset

--- terraform/route_53.tf
+++
@@ -1,7 +0,0 @@
-resource "aws_route53_record" "mc" {
-  zone_id = var.route_53_zone_id
-  name    = "mc.${var.domain_name}"
-  type    = "A"
-  ttl     = 172800
-  records = [aws_instance.mozzy_craft_ec2.public_ip]
-}
