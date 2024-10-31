8a5c96b598d8570da9534b54a174e4f0911eeb37
Aditya Tapshalkar <aditya.taps@gmail.com>
2024-10-31 01:33:59 -0400

    initial lambda commit

--- opentofu/api.tf
+++ opentofu/api.tf
@@ -1,2 +1,29 @@
 # api.tf
 
+locals {
+  lambda_name = "mozzycraft_api_controller"
+}
+
+### Lambda
+data "aws_iam_policy_document" "lambda" {
+  statement {
+    effect = "Allow"
+
+    principals {
+      type        = "Service"
+      identifiers = ["lambda.amazonaws.com"]
+    }
+
+    actions = ["sts:AssumeRole"]
+  }
+}
+
+resource "aws_iam_role" "lambda" {
+  name               = "iam_for_lambda"
+  assume_role_policy = data.aws_iam_policy_document.lambda.json
+}
+
+resource "aws_lambda_function" "lambda" {
+  function_name = local.lambda_name
+  role          = aws_iam_role.lambda.arn
+}
